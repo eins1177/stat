bf015f870e8939d2ebae08bbb503a22079fafad4
ruban link <cshlove11@naver.com>
2023-09-25 21:55:19 +0900

    다시 연습

--- main.py
+++ main.py
@@ -1,43 +1,8 @@
-# -*- coding:utf-8 -*-
-import streamlit as st 
-import matplotlib.pyplot as plt 
-import seaborn as sns 
-import numpy as np 
+import streamlit as st
 
-# 한글폰트 적용
-# 폰트 적용
-import os
-import matplotlib.font_manager as fm  
+st.header('st.button')
 
-def unique(list):
-    x = np.array(list)
-    return np.unique(x)
-
-@st.cache_data
-def fontRegistered():
-    font_dirs = [os.getcwd() + '/customFonts']
-    font_files = fm.findSystemFonts(fontpaths=font_dirs)
-
-    for font_file in font_files:
-        fm.fontManager.addfont(font_file)
-    fm._load_fontmanager(try_read_cache=False)
-    
-
-def main():
-    
-    fontRegistered()
-    fontNames = [f.name for f in fm.fontManager.ttflist]
-    fontname = st.selectbox("폰트 선택", unique(fontNames))
-
-    plt.rc('font', family=fontname)
-    tips = sns.load_dataset("tips")
-    fig, ax = plt.subplots()
-    sns.scatterplot(data=tips, x = 'total_bill', y = 'tip', hue='day')
-    ax.set_title("한글 테스트")
-    st.pyplot(fig)
-    
-    st.dataframe(tips)
-    
-
-if __name__ == "__main__":
-    main()
+if st.button('안녕'):
+     st.write('왜 거기 있어')
+else:
+     st.write('Goodbye')
